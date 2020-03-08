228655bc2578dda2ee35bbcd7b81656fb72174c7
Alejandro Flores <alejandro@esporaestudio.com>
2020-03-07 18:50:31 -0600

    Se agrega la función de crear posts usando Ajax

--- controllers/HomepageController.js
+++ controllers/HomepageController.js
@@ -2,6 +2,9 @@ const Task = require('../models/Task');
 
 exports.index = (req, res) => {
   let tasks = Task.all().then((tasks) => {
+    tasks.sort((a, b) => {
+      return b.id - a.id;
+    });
     res.render('homepage/index', {tasks: tasks});
   });
 }
--- controllers/TasksController.js
+++ controllers/TasksController.js
@@ -15,7 +15,11 @@ exports.store = (req, res) => {
   let task = {};
   task.description = req.body.description;
   Task.create(task).then((id) => {
-    console.log('Task created with id: ', id);
-    res.redirect('/');
+    // if the request is expecting an ajax or json response
+    if (req.xhr || req.headers.accept.indexOf('json') > -1) {
+      Task.find(id).then((task) => res.json(task));
+    } else {
+      res.redirect('/');
+    }
   });
 }
